f336e4698ed6e49599cd1b6bb0898ba1f447db1e
mac <mac@macs-MacBook-Pro.local>
2024-02-26 12:54:36 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App h-screen w-screen bg-black flex justify-center items-center">
+
+      <div className='border-2 border-orange-500 px-28 py-20 rounded-lg'>
+        <div>
+        <h1 className='text-orange-500 font-bold text-5xl animate-bounce duration-700'>Tes bang <span className='text-orange-500 text-8xl'>!</span></h1>      
+
+        </div>
+      </div>
+
     </div>
   );
 }
